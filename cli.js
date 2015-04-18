ec348ea70104f8d8ea07842289f5d1e9987fd837
David Konsumer <konsumer@jetboystudio.com>
2015-04-18 09:15:00 -0700

    bump

--- cli.js
+++ cli.js
@@ -33,7 +33,7 @@ if (argv._.indexOf('in') !== -1){
   var output = new midi.output();
   output.openVirtualPort('jetmidi-out');
   socket.on('message', function(msg){
-    console.log('message', msg);
+    console.log('message', msg.message);
     output.sendMessage(msg);
   });
 }
